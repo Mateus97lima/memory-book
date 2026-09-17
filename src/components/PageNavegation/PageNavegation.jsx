82ceaf6fbd3479b762b90349dec9f409bc25fefb
Mateus Lima <matheus2022y@gmail.com>
2026-09-17 19:53:06 -0300

    alguns ajuste

--- src/components/PageNavegation/PageNavegation.jsx
+++ src/components/PageNavegation/PageNavegation.jsx
@@ -76,7 +76,7 @@ export default function PageNavigation({
           </button>
         </div>
 
-        {/* Center: Search & Filter Toggle */}
+        
         <div className="flex items-center gap-2 flex-1 max-w-md mx-auto">
           <div className="relative w-full">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-stone-400" />
